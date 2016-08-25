e05a32fc9432697317f06d0a456d742a5b23e1f7
ernsnl <ernsnl@gmail.com>
2016-08-26 01:23:03 +0300

    Basic Class Structure

--- console/js/console.js
+++ console/js/console.js
@@ -1,20 +1,67 @@
 (function($) {
 
-    $.fn.console = function(options) {
-
-
-        // This is the easiest way to have default options.
-        var settings = $.extend({}, $.fn.console.defaults, options);
-
-        return this.each(function() {
-            var elem = $(this);
-            $.fn.console.create(elem, settings);
-            setCursorIndicator(elem);
-        });
-
+    // Console Class Definition
+    var ConsoleCls = function(obj, settings) {
+        this.console_elem = obj;
+        this.console_settings = settings;
+        this.init();
     };
 
-}(jQuery));
+    // Init a Class which will create the Console
+    ConsoleCls.prototype.init = function() {
+        this.defineProperties();
+        $(this.console_elem).append(this.console_settings.console_welcome_message);
+        $(this.console_elem).append("<p class='command-line'>" + "<span class='console_directory_message'>" + this.console_settings.console_directory_message + ": </span>" + "</p>");
+
+        //Adding Element for Necessary Console Behaviors
+        this.addInputBox();
+        this.setCursorIndicator();
+    };
+
+    // For Defining CSS Properties
+    ConsoleCls.prototype.defineProperties = function() {
+        if (this.console_settings.use_custom_height)
+            $(this.console_elem).css("height", this.console_settings.custom_height);
+        else
+            $(this.console_elem).css("height", 400);
+
+        $(this.console_elem).addClass(this.console_settings.class_name);
+    };
+
+    ConsoleCls.prototype.addInputBox = function() {
+        if ($(this.console_elem).find(".console-input-box").length > 0) {
+            $(this.console_elem).find(".console-input-box").remove();
+        } else {
+            $(this.console_elem).find(".command-line").append("<input type='text' class='console-input-box'/>");
+            $(this.console_elem).find(".console-input-box").keypress(function(event) {
+                this.handleKeyPress(event);
+            });
+        }
+    };
+
+    // Creating a cursor indicator for console application
+    ConsoleCls.prototype.setCursorIndicator = function() {
+        if ($(this.console_elem).find(".cursor-indicator").length > 0) {
+            $(this.console_elem).find(".cursor-indicator").remove();
+        } else {
+            $(this.console_elem).find(".command-line").append("<span class='cursor-indicator active-console'></span>");
+        }
+    };
+
+
+    ConsoleCls.prototype.handleKeyPress = function(event) {
+        console.log('Key Pressed');
+    };
+
+    $.fn.console = function(options) {
+        // This is the easiest way to have default options.
+        var settings = $.extend({}, $.fn.console.defaults, options);
+        return this.each(function() {
+            var console = new ConsoleCls(this, settings);
+        });
+    };
+
+})(jQuery);
 
 // Defaults settings
 $.fn.console.defaults = {
@@ -31,29 +78,3 @@ $.fn.console.defaults = {
     use_custom_height: false,
     custom_height: 0,
 };
-
-$.fn.console.create = function(elem, settings) {
-    $.fn.console.define_properties(elem, settings);
-
-    elem.append(settings.console_welcome_message);
-    elem.append("<p class='command-line'>" + "<span class='console_directory_message'>" + settings.console_directory_message + ": </span>" + "</p>");
-};
-
-$.fn.console.define_properties = function(elem, settings) {
-    if (elem.innerWidth() < 600)
-        throw "Element width cannot be smaller than 600 px";
-    if (settings.use_custom_height)
-        elem.css("height", settings.custom_height);
-    else
-        elem.css("height", 400);
-
-    elem.addClass(settings.class_name);
-};
-
-function setCursorIndicator(elem) {
-    if ($(elem).find(".cursor-indicator").length > 0) {
-        $(elem).find(".cursor-indicator").remove();
-    } else {
-        $(elem).find(".command-line").append("<span class='cursor-indicator'>B</span>");
-    }
-}
--- console/js/main.js
+++ console/js/main.js
@@ -1,19 +1,4 @@
 
-
-
-function cursorIndicator() {
-    if ($(".cursor-indicator").length)
-        $(".cursor-indicator").remove();
-    else
-        $(".command-input").append("<span class='cursor-indicator'>B</span>");
-    setTimeout(cursorIndicator, 250);
-}
-
-function insertLine(document, event) {
-    var input = $(document);
-    processByEvent(input, event);
-}
-
 function processByEvent(document, event) {
     // TO-DO Events:
     // TAB
